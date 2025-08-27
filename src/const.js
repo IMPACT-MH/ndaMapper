3a80b9beb49f7c1fbdb35d3cfdd156058bbe0baf
joshkenney <joshuagkenney@gmail.com>
2025-08-27 16:14:25 -0400

    using export data-portal route

--- src/const.js
+++ src/const.js
@@ -1,2 +1,4 @@
-export const DATA_PORTAL =
-    "https://spinup-002b0f.spinup.yale.edu/api/dataStructures/database";
+// export const DATA_PORTAL =
+// "https://spinup-002b0f.spinup.yale.edu/api/dataStructures/database";
+
+export const DATA_PORTAL = "https://api.impact-mh.org/impact/data-portal";
